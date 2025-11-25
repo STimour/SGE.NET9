c7e57121a572ff606d3cd35c88a8f69c46d3368a
Tim <121019458+STimour@users.noreply.github.com>
2025-11-25 09:51:58 +0000

    wip: correction

--- SGE.Core/Exception/SgeException.cs
+++ SGE.Core/Exception/SgeException.cs
@@ -1,6 +1,32 @@
 namespace SGE.Core.Exception;
 
-public class SgeException
+public class SgeException : System.Exception
 {
-    
+	public string ErrorCode { get; }
+	public int StatusCode { get; }
+
+	public SgeException()
+	{
+		ErrorCode = "SGE_ERROR";
+		StatusCode = 500;
+	}
+
+	public SgeException(string message) : base(message)
+	{
+		ErrorCode = "SGE_ERROR";
+		StatusCode = 500;
+	}
+
+	public SgeException(string message, string errorCode, int statusCode) : base(message)
+	{
+		ErrorCode = errorCode ?? "SGE_ERROR";
+		StatusCode = statusCode;
+	}
+
+	public SgeException(string message, Exception innerException, string errorCode = "SGE_ERROR", int statusCode = 500)
+		: base(message, innerException)
+	{
+		ErrorCode = errorCode;
+		StatusCode = statusCode;
+	}
 }
